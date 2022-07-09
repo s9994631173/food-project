655eec0f28a33de5b40ec07c4b4a84f442d925dc
Sergey <s9994631173@gmail.com>
2022-07-09 13:29:37 +0700

    auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,6 +16,8 @@ class LoginController extends Controller
         $validated = $request->safe()->only(['email', 'password']);
 
         if (Auth::attempt($validated, true)) {
+            $request->session()->regenerate();
+            
             $user = Auth::user();
 
             return [
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -53,9 +54,10 @@ class User extends Authenticatable
             ]];
         }else{
             $user = self::create($validated);
+            Auth::login($user);
+            $username = Auth::user()->name;
 
-            $token = $user->createToken('user-token');
-            return ['token' => $token->plainTextToken];
+            return ['user name' => $username];
         }
     }
 }
