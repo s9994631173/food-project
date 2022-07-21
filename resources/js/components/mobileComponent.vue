4a1d0c757bd33d033f092c6db0209b5972e89de5
s9994631173 <75929627+s9994631173@users.noreply.github.com>
2022-07-22 01:28:26 +0700

    test

--- resources/js/components/mobileComponent.vue
+++ resources/js/components/mobileComponent.vue
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
         <div class="col-12 gy-4">
             <div class="row justify-content-center">
-                <div class="col-4 text-center">
+                <div class="col text-center">
                     <Weight/>
                 </div>
             </div>
